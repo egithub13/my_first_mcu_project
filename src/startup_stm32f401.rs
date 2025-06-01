4e4630e0b2c0b2e948401cdf1200e70f205f9aa9
Eric Hayes <egithub13@gmail.com>
2025-05-31 23:14:32 -0400

    Create linker file and revise sections

---
+++ src/startup_stm32f401.rs
@@ -0,0 +1,11 @@
+//1. Define vector table
+
+//2. Define reset handler
+fn reset_handler() {
+    //1. Copy the .data section from FLASH to RAM
+    //2. Zero out the .bss section iin the RAM
+    //3. Call main()
+    
+}
+
+//3. Define exception handlers
---
+++ target/thumbv7em-none-eabihf/debug/deps/my_first_mcu_project-1a22e97fcbc51e46.d
@@ -0,0 +1,5 @@
+/Users/erichayes/Desktop/Projects/Rust/embedded-rust/my_first_mcu_project/target/thumbv7em-none-eabihf/debug/deps/my_first_mcu_project-1a22e97fcbc51e46: src/main.rs
+
+/Users/erichayes/Desktop/Projects/Rust/embedded-rust/my_first_mcu_project/target/thumbv7em-none-eabihf/debug/deps/my_first_mcu_project-1a22e97fcbc51e46.d: src/main.rs
+
+src/main.rs:
