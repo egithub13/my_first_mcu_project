efb41cc8b99a85887d8c86dc834e1bed61a7c80e
Eric Hayes <egithub13@gmail.com>
2025-06-03 12:10:43 -0400

    Revised launch.json and settings.json file for debugging issues. Fixed.

--- target/thumbv7em-none-eabihf/debug/deps/my_first_mcu_project-1a22e97fcbc51e46.d
+++
@@ -1,6 +0,0 @@
-C:\Users\eric.hayes\source\repos\Rust\my_first_mcu_project\target\thumbv7em-none-eabihf\debug\deps\my_first_mcu_project-1a22e97fcbc51e46: src\main.rs src\startup_stm32f401.rs
-
-C:\Users\eric.hayes\source\repos\Rust\my_first_mcu_project\target\thumbv7em-none-eabihf\debug\deps\my_first_mcu_project-1a22e97fcbc51e46.d: src\main.rs src\startup_stm32f401.rs
-
-src\main.rs:
-src\startup_stm32f401.rs:
